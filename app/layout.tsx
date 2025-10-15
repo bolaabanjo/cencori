ce3f07156b1fe94426fbcfd224b8d1c68d5e4962
bolaabanjo <royadebanjo@gmail.com>
2025-10-15 10:26:09 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Navbar from "@/components/navbar"; // Import the Navbar component
 
