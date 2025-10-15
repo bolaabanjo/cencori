fc673152bd68ed915eec166560a3ed4703589a68
bolaabanjo <royadebanjo@gmail.com>
2025-10-15 07:01:51 +0100

    .

--- components/ui/container-text-flip.tsx
+++ components/ui/container-text-flip.tsx
@@ -33,7 +33,7 @@ export function ContainerTextFlip({
   const updateWidthForWord = () => {
     if (textRef.current) {
       // Add some padding to the text width (30px on each side)
-      // @ts-expect-error
+      // @ts-expect-error: `textRef.current` can be null initially or if the ref is not attached.
       const textWidth = textRef.current.scrollWidth + 30;
       setWidth(textWidth);
     }
