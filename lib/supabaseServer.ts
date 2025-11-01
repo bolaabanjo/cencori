24188f8a7650b785af1bfb211f66bd04a140304b
bolaabanjo <royadebanjo@gmail.com>
2025-11-01 08:01:23 +0100

    edited lib files to fit in the new organization changes

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,8 +1,39 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createServerClient as createSSClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createServerClient = () => {
-    return createServerComponentClient({
-        cookies,
-    });
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+export const createServerClient = async () => {
+  const cookieStore = await cookies();
+
+  return createSSClient(
+    supabaseUrl!,
+    supabaseKey!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set(name, value, options);
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set(name, "", { ...options, maxAge: -1 });
+          } catch (error) {
+            // The `remove` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    },
+  );
 };
